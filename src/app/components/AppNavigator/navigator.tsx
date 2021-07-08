aba97865ad1e27cfec6463cfa3c7ecf4c6fa5154
Andy Olarte <andy.olarte514@gmail.com>
2021-07-08 09:19:21 -0500

    "Kickoff!"

---
+++ src/app/components/AppNavigator/navigator.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useSelector } from 'react-redux';
+import { State } from '@interfaces/reduxInterfaces';
+import Routes from '@constants/routes';
+import { authStackNavConfig, appStackNavConfig, tabNavConfig } from '@config/navigation';
+import { inferRoute } from '@utils/navUtils';
+import Login from '@authScreens/Login';
+import SignUp from '@authScreens/SignUp';
+import OnBoarding from '@screens/OnBoarding';
+import Home from '@screens/Home';
+
+const Stack = createStackNavigator();
+
+const AuthStack = () => (
+  <>
+    {inferRoute(Stack)({ [Routes.Login]: Login })}
+    {inferRoute(Stack)({ [Routes.SignUp]: SignUp })}
+  </>
+);
+
+const Tab = createBottomTabNavigator();
+function HomeTabs() {
+  return (
+    <Tab.Navigator {...tabNavConfig}>
+      {inferRoute(Tab)({ [Routes.Tab1]: Home })}
+      {inferRoute(Tab)({ [Routes.Tab2]: Home })}
+    </Tab.Navigator>
+  );
+}
+
+function AppStack() {
+  return <>{inferRoute(Stack)({ [Routes.Home]: HomeTabs })}</>;
+}
+
+const OnBoardingStack = () => <>{inferRoute(Stack)({ [Routes.OnBoarding]: OnBoarding })}</>;
+
+const Navigator = () => {
+  const hasAccessOnBoarding = useSelector((state: State) => state.auth.hasAccessOnBoarding);
+  const currentUser = useSelector((state: State) => state.auth.currentUser);
+  const defaultStackConfig = currentUser ? appStackNavConfig : authStackNavConfig;
+  return (
+    <Stack.Navigator {...defaultStackConfig}>
+      {currentUser ? (hasAccessOnBoarding ? AppStack() : OnBoardingStack()) : AuthStack()}
+    </Stack.Navigator>
+  );
+};
+
+export default Navigator;
---
+++ src/constants/routes.ts
@@ -0,0 +1,14 @@
+import { StringObject } from '@interfaces/globalInterfaces';
+
+const Routes: StringObject = {
+  Auth: 'Auth',
+  Login: 'Login',
+  SignUp: 'SignUp',
+  OnBoarding: 'OnBoarding',
+  Tab1: 'Tab1',
+  Tab2: 'Tab2',
+  App: 'App',
+  Home: 'Home'
+};
+
+export default Routes;
