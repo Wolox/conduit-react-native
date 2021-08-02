41e46ce5bdfdf0f37778b8af830e6ca4889f5913
Andy Olarte <andy.olarte514@gmail.com>
2021-08-02 15:42:57 -0500

    change translations

--- src/app/screens/Profile/i18n.ts
+++ src/app/screens/Profile/i18n.ts
@@ -1,7 +1,7 @@
 import i18next from 'i18next';
 
 i18next.addResources('en', 'PROFILE', {
-  LOGOUT: 'Cerrar sesión',
+  LOGOUT: 'Logout',
   USERNAME: 'Username',
   EMAIL: 'E-mail',
   DESCRIPTION: 'Description about you',
