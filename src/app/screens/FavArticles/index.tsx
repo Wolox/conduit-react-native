bccb1e3f8d9826c778d029327074d0d11c383f11
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-08-03 16:32:47 -0300

    fix styles

--- src/app/screens/FavArticles/index.tsx
+++ src/app/screens/FavArticles/index.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useCallback } from 'react';
 import { SafeAreaView, FlatList, ListRenderItem, View } from 'react-native';
-import FavouritesActions from '@redux/favourites/actions';
 import { useDispatch, useSelector } from 'react-redux';
+import i18next from 'i18next';
+import FavouritesActions from '@redux/favourites/actions';
 import { State } from '@interfaces/reduxInterfaces';
 import { Article } from '@interfaces/articlesInterface';
 import ArticleItem from '@components/ArticleItem';
 import { ListKeyExtractor } from '@interfaces/miscelanious';
 import ScreenWithLoader from '@components/ScreenWithLoader';
+import CustomText from '@components/CustomText';
 
 import './i18n';
 import styles from './styles';
@@ -24,6 +26,11 @@ export default function FavArticles() {
   }, [dispatch]);
   return (
     <SafeAreaView style={styles.container}>
+      <View style={styles.statusNavBar}>
+        <View style={styles.textTitle}>
+          <CustomText medium>{i18next.t('FAV_ARTICLES:FAVOURITED_ARTICLES')}</CustomText>
+        </View>
+      </View>
       <ScreenWithLoader
         loading={favouritesarticlesListLoading || !favouritesarticlesList.length}
         withInitialLoading={false}>
--- src/app/screens/FavArticles/styles.ts
+++ src/app/screens/FavArticles/styles.ts
@@ -1,8 +1,10 @@
 import { StyleSheet } from 'react-native';
 import { BOTTOM_SEPARATOR_LINE } from '@constants/commonStyles';
 import { isIos } from '@constants/platform';
+import { green } from '@constants/colors';
 
 const MARGIN_BOTTOM_LIST = isIos ? 35 : 55;
+const MARGIN_TOP_TITLE = isIos ? 2 : 20;
 const styles = StyleSheet.create({
   container: {
     flex: 1
@@ -14,6 +16,17 @@ const styles = StyleSheet.create({
     ...BOTTOM_SEPARATOR_LINE,
     width: '100%',
     alignItems: 'center'
+  },
+  statusNavBar: {
+    marginTop: MARGIN_TOP_TITLE,
+    width: '100%',
+    alignSelf: 'center',
+    alignItems: 'center'
+  },
+  textTitle: {
+    borderBottomColor: green,
+    borderBottomWidth: 2,
+    marginBottom: -1
   }
 });
 export default styles;
