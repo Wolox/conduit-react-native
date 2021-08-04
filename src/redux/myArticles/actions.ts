d122ddb00d8008cf3f6eae50128765d409874a7c
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-08-04 16:07:21 -0300

    init redux actions/reducer

---
+++ src/redux/myArticles/actions.ts
@@ -0,0 +1,25 @@
+import { Dispatch } from 'react';
+import { createTypes, completeTypes } from 'redux-recompose';
+import { Action, DispatcheableAction } from '@interfaces/reduxInterfaces';
+import { Nullable } from '@interfaces/globalInterfaces';
+import MyArticlesService from '@services/MyArticlesService';
+
+export const actions = createTypes(completeTypes({ primaryActions: ['MY_ARTICLES'] }), '@@MY_ARTICLES');
+
+export const TARGETS = {
+  MY_ARTICLES: 'myArticles'
+};
+
+type ActionType = Nullable<number>;
+
+const actionCreators = {
+  getMyArticles: () => (dispatch: Dispatch<Action<ActionType> | DispatcheableAction<ActionType>>) => {
+    dispatch({
+      type: actions.MY_ARTICLES,
+      target: TARGETS.MY_ARTICLES,
+      service: MyArticlesService.getMockMyArticles
+    });
+  }
+};
+
+export default actionCreators;
