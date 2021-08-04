cb43e5813eb7597a2635d333fd7483e823ad648d
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-08-04 09:55:54 -0300

    fixed translated

--- src/app/screens/Auth/screens/SignUp/i18n.ts
+++ src/app/screens/Auth/screens/SignUp/i18n.ts
@@ -4,7 +4,7 @@ i18next.addResources('en', 'SIGNUP', {
   USERNAME: 'Username',
   USERNAME_PLACEHOLDER: 'Hello12',
   MAIL: 'Email',
-  MAIL_PLACEHOLDER: 'Ej: email@dominio.com',
+  MAIL_PLACEHOLDER: 'Ex: email@dominio.com',
   PASSWORD: 'Password',
   SIGN_UP: 'Sign up',
   SIGNUP_FAILURE: 'An error occurred. Please try again!'
