1d36c9dedf6b31b62e38aa7b83b0c33e22fc26ad
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-08-23 12:23:34 -0300

    clean favouritesactions imports

--- src/redux/favourites/actions.ts
+++ src/redux/favourites/actions.ts
@@ -1,8 +1,6 @@
 import { createTypes, completeTypes } from 'redux-recompose';
-import { Nullable } from '@interfaces/globalInterfaces';
 import FavoriteService from '@services/FavouritesArticlesService';
 import { CurrentUser } from '@interfaces/authInterfaces';
-// import { CurrentUser } from '@interfaces/authInterfaces';
 
 export const actions = createTypes(
   completeTypes({ primaryActions: ['FAVOURITES_ARTICLES_LIST'] }),
@@ -12,8 +10,6 @@ export const actions = createTypes(
 export const TARGETS = {
   FAVOURITES_ARTICLES_LIST: 'favouritesarticlesList'
 };
-
-type ActionType = Nullable<number>;
 
 const actionCreators = {
   getFavouritesArticles: (currentUser: CurrentUser) => ({
