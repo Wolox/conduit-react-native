15e087e60ee289b92b7c276c0b0f44f3a6d152df
Andres Felipe Zuluaga Arias <75996952+andreszuluaga-wolox@users.noreply.github.com>
2021-09-06 10:21:53 -0500

    Edit profile (#21)
    * Add nameuser validation and update currentUser
    
    * Add updateCurrentUser and validate username
    
    * Fix comments
    
    * Fix more comments

--- src/redux/auth/actions.ts
+++ src/redux/auth/actions.ts
@@ -2,19 +2,23 @@ import { ApiOkResponse } from 'apisauce';
 import { Dispatch } from 'react';
 import { createTypes, completeTypes, withPostSuccess, withPostFailure } from 'redux-recompose';
 import { setApiHeaders, removeApiHeaders } from '@config/api';
-import { CurrentUser, AuthData, UserResponse } from '@interfaces/authInterfaces';
+import { CurrentUser, AuthData, UserResponse, ProfileData } from '@interfaces/authInterfaces';
 import { Nullable } from '@interfaces/globalInterfaces';
 import { Action, State } from '@interfaces/reduxInterfaces';
-import { login, logout } from '@services/AuthService';
+import { login, logout, updateCurrentUser, getUserProfile } from '@services/AuthService';
 
 export const actions = createTypes(
-  completeTypes({ primaryActions: ['LOGIN', 'LOGOUT'], ignoredActions: ['AUTH_INIT', 'HAS_ACCESS'] }),
+  completeTypes({
+    primaryActions: ['LOGIN', 'LOGOUT', 'UPDATE_CURRENT_USER', 'GET_USER_PROFILE'],
+    ignoredActions: ['AUTH_INIT', 'HAS_ACCESS']
+  }),
   '@@AUTH'
 );
 
 const TARGETS = {
   ONBOARDING: 'hasAccessOnBoarding',
-  CURRENT_USER: 'currentUser'
+  CURRENT_USER: 'currentUser',
+  USER_PROFILE: 'userProfile'
 };
 
 export const actionCreators = {
@@ -71,5 +75,32 @@ export const actionCreators = {
     type: actions.HAS_ACCESS,
     target: TARGETS.ONBOARDING,
     payload: value
+  }),
+  updateCurrentUser: (user: UserResponse, failedFunction: Function) => ({
+    type: actions.UPDATE_CURRENT_USER,
+    target: TARGETS.CURRENT_USER,
+    payload: user,
+    service: updateCurrentUser,
+    injections: [
+      withPostFailure((_dispatch: Dispatch<any>) => {
+        failedFunction();
+      })
+    ]
+  }),
+  getUserProfile: (user: UserResponse, successFunction: Function, updateFailed: Function) => ({
+    type: actions.GET_USER_PROFILE,
+    target: TARGETS.USER_PROFILE,
+    payload: user.user?.username,
+    service: getUserProfile,
+    injections: [
+      withPostSuccess((_: Dispatch<any>, response: ApiOkResponse<ProfileData>) => {
+        if (response.data?.profile?.username === user.user?.username) {
+          successFunction();
+        }
+      }),
+      withPostFailure((dispatch: Dispatch<any>) => {
+        dispatch(actionCreators.updateCurrentUser(user, updateFailed));
+      })
+    ]
   })
 };
