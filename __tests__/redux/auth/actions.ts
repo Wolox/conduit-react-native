1eb4531ac70f399d8c1c264a8befb57732928f3a
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-09-15 18:57:41 -0300

    added authUnitTest, fixed imports and actions

---
+++ __tests__/redux/auth/actions.ts
@@ -0,0 +1,46 @@
+import { fetchMiddleware } from 'redux-recompose';
+import { actionCreators, actions, TARGETS } from '@redux/auth/actions';
+import { actions as feedbackactions } from '@redux/feedback/actions';
+import thunk from 'redux-thunk';
+import configureStore from 'redux-mock-store';
+import api from '@config/api';
+import { initialState } from '@redux/auth/reducer';
+
+import { mapActionsToTypes } from '../utils';
+
+const middleware = [thunk, fetchMiddleware];
+const mockStore = configureStore(middleware);
+const store = mockStore(initialState);
+
+describe('testing auth actions', () => {
+  beforeEach(() => {
+    store.clearActions();
+  });
+  test('test AUTH_INIT', async () => {
+    await store.dispatch(actionCreators.init());
+    expect(store.getActions()).toEqual([{ target: TARGETS.CURRENT_USER, type: actions.AUTH_INIT }]);
+  });
+
+  test('test LOGIN ERROR AND INJECTIONS', async () => {
+    const FAIL_USER = { email: 'fail22@fail.com', password: 'hola1234' };
+    api.setHeader = jest.fn();
+    const expectedActions = [actions.LOGIN, actions.LOGIN_FAILURE, feedbackactions.SHOW_MODAL];
+    await store.dispatch(actionCreators.login(FAIL_USER));
+    expect(mapActionsToTypes(store.getActions())).toEqual(expectedActions);
+  });
+  test('test LOGIN SUCCESS', () => {
+    const renderUser = () => {
+      const MOCKED_USER = { email: 'hola5@hola.com', password: 'hola1234' };
+      store.dispatch(actionCreators.login(MOCKED_USER));
+    };
+    renderUser();
+    expect(store.getActions()).toEqual([{ target: TARGETS.CURRENT_USER, type: actions.LOGIN }]);
+  });
+  test('test LOGOUT ', async () => {
+    const spy = jest.spyOn(store, 'dispatch');
+    store.dispatch(actionCreators.logout());
+    expect(store.getActions()).toEqual([{ target: TARGETS.CURRENT_USER, type: actions.LOGOUT }]);
+    expect(await store.getState()).toEqual(initialState);
+    expect(spy).toBeCalled();
+  });
+});
--- jest.config.js
+++ jest.config.js
@@ -8,9 +8,13 @@ module.exports = {
     './node_modules/react-native-gesture-handler/jestSetup.js',
     '@testing-library/jest-native/extend-expect'
   ],
+  // TODO: UNCCOMMENT OR EDIT IF WE'LL NEED SOME OF THIS MODULES
+  // transformIgnorePatterns: [
+  //   '/node_modules/@react-native-async-storage/async-storage/(?!(lib))',
+  //   'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@react-native-firebase/app)'
+  // ],
   transformIgnorePatterns: [
-    '/node_modules/@react-native-async-storage/async-storage/(?!(lib))',
-    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@react-native-firebase/app)'
+    'node_modules/(?!(jest-)?@react-native|react-native|react-clone-referenced-element/*)'
   ],
   moduleNameMapper: {
     '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
