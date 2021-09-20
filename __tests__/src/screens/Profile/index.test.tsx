90ea61304635c6b7e935fdc616f444cfddbe8d29
andres.f.zuluaga <andres.zuluaga@wolox.co>
2021-09-20 09:21:22 -0500

    Fix test from Profile

--- __tests__/src/screens/Profile/index.test.tsx
+++ __tests__/src/screens/Profile/index.test.tsx
@@ -3,18 +3,19 @@ import renderer from 'react-test-renderer';
 import * as redux from 'react-redux';
 import Profile from '@screens/Profile';
 
-const useSelectorMock = jest.spyOn(redux, 'useSelector');
-useSelectorMock.mockReturnValue({
-  userProfileLoading: false,
-  currentUserLoading: false,
-  currentUserError: null
-});
-
 describe('Profile', () => {
-  test('Profile Snapshot', async done => {
-    const tree = await renderer.create(<Profile />).toJSON();
+  beforeEach(() => {
+    const useSelectorMock = jest.spyOn(redux, 'useSelector');
+    useSelectorMock.mockReturnValue({
+      userProfileLoading: false,
+      currentUserLoading: false,
+      currentUserError: null
+    });
+    jest.useFakeTimers();
+  });
+  test('Profile Snapshot', () => {
+    const tree = renderer.create(<Profile />).toJSON();
     expect(tree).toMatchSnapshot();
     expect(tree).not.toBeNull();
-    done();
   });
 });
