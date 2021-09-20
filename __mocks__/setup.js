af4cc467ed4aab97dea7adb02595f1dafe0012de
john07289 <jhon.vente@wolox.co>
2021-09-20 14:28:58 -0500

    snapshot for Newarticle screen

--- __mocks__/setup.js
+++ __mocks__/setup.js
@@ -1,3 +1,7 @@
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('react-native-keyboard-aware-scroll-view', () => {
+  const KeyboardAwareScrollView = ({ children }) => children;
+  return { KeyboardAwareScrollView };
+});
 global.window = {};
 global.window = global;
