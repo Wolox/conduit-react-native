41568cbd0ac78cb23d9a77de5d67ec0c9d38c75c
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-09-23 11:54:03 -0300

    changed handlePress CrossBack

--- src/app/components/CrossBack/index.tsx
+++ src/app/components/CrossBack/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { NavigationProp } from '@react-navigation/core';
 import { onResetStack } from '@utils/navUtils';
+import { useDispatch } from 'react-redux';
+import actionCreators from '@redux/articles/actions';
 
 import styles from './styles';
 
@@ -10,8 +12,15 @@ type Props = {
 };
 
 export default function CrossBack({ navigation }: Props) {
+  const dispatch = useDispatch();
+  const handlePress = () => {
+    setTimeout(async () => {
+      await onResetStack(navigation, []);
+      await dispatch(actionCreators.getArticles());
+    }, 500);
+  };
   return (
-    <TouchableOpacity onPress={() => onResetStack(navigation, [])}>
+    <TouchableOpacity onPress={handlePress}>
       <View style={styles.container}>
         <Text>X</Text>
       </View>
