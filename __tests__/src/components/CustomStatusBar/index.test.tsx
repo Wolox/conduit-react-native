a743a31ff3ff7544fc0253130207b648181ab80a
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-09-28 16:27:52 -0300

    fixed imports

--- __tests__/src/components/CustomStatusBar/index.test.tsx
+++ __tests__/src/components/CustomStatusBar/index.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import MockedNavigator from '@mocks/MockedNavigator';
-
-import Routes from '../../../../src/constants/routes';
-import CustomStatusBar from '../../../../src/app/components/CustomStatusBar';
+import Routes from '@constants/routes';
+import CustomStatusBar from '@components/CustomStatusBar';
 
 describe('customStatusBar', () => {
   test('customStatusBar passedAllProps', () => {
@@ -11,9 +10,11 @@ describe('customStatusBar', () => {
       renderTabList: () => <CustomStatusBar routeName={Routes.Home} />,
       renderCompleteTabList: () => <MockedNavigator component={RenderCustom.renderTabList} />
     };
-    // const treeInstance = renderer.create(RenderCustom.renderCompleteTabList());
+    const treeInstance = renderer.create(RenderCustom.renderCompleteTabList());
     const tree = renderer.create(RenderCustom.renderCompleteTabList()).toJSON();
     expect(tree).toMatchSnapshot();
     expect(tree).not.toBeNull();
+    expect(treeInstance.root.findByType(CustomStatusBar).props.routeName).not.toBeNull();
+    expect(treeInstance.root.findByType(CustomStatusBar).props.routeName).toBe(Routes.Home);
   });
 });
