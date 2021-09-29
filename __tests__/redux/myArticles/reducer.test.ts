2d97ffb935a94ca2dd5de505adcb535ca694b5c4
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-09-29 15:03:17 -0300

    added myArticles Test

---
+++ __tests__/redux/myArticles/reducer.test.ts
@@ -0,0 +1,52 @@
+import { actions, TARGETS } from '@redux/myArticles/actions';
+import MyArticlesReducer, { initialState } from '@redux/myArticles/reducer';
+import { CurrentUser } from '@interfaces/authInterfaces.ts';
+
+const MOCKED_DATA: CurrentUser = {
+  bio: 'Esta es una Biografía típica de un usuario !',
+  email: 'hola5@hola.com',
+  id: 209,
+  image:
+    'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+  token:
+    'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MjA5LCJleHAiOjE2MzY5MDMzNzZ9.zN6uihJ-GSArwOpwFUHqLOgNGfTsyIJtoVB4JLlMlTQ',
+  username: 'hola5'
+};
+const myArticlesActions = {
+  getFavouritesArticles: {
+    type: actions.MY_ARTICLES,
+    target: TARGETS.MY_ARTICLES
+  },
+  getFavouritesArticlesSuccess: {
+    type: actions.MY_ARTICLES_SUCCESS,
+    target: TARGETS.MY_ARTICLES,
+    payload: MOCKED_DATA
+  },
+  getFavouritesArticlesFailure: {
+    type: actions.MY_ARTICLES_FAILURE,
+    target: TARGETS.MY_ARTICLES,
+    payload: true
+  }
+};
+
+describe('case myArticlesReducer', () => {
+  test('test GET_MY_ARTICLES_INIT', () => {
+    expect(MyArticlesReducer(undefined, myArticlesActions.getFavouritesArticles)).toMatchObject({
+      ...initialState,
+      myArticlesLoading: true
+    });
+  });
+  test('test GET_MY_ARTICLES_SUCCESS', async () => {
+    expect(await MyArticlesReducer(undefined, myArticlesActions.getFavouritesArticlesSuccess)).toMatchObject({
+      ...initialState,
+      myArticles: MOCKED_DATA
+    });
+    expect(await MyArticlesReducer(undefined, myArticlesActions.getFavouritesArticlesSuccess)).not.toBeNull();
+  });
+  test('test GET_MY_ARTICLES_FAILURE ', async () => {
+    expect(await MyArticlesReducer(undefined, myArticlesActions.getFavouritesArticlesFailure)).toMatchObject({
+      ...initialState,
+      myArticlesError: true
+    });
+  });
+});
