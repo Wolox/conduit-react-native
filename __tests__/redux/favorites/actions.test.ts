47e6755204d85f5a59ece7065612682f212d2c75
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-09-29 12:43:40 -0300

    added redux/favorites unitTest

---
+++ __tests__/redux/favorites/actions.test.ts
@@ -0,0 +1,47 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { fetchMiddleware } from 'redux-recompose';
+import actionCreators, { actions, TARGETS } from '@redux/favourites/actions';
+import { initialState } from '@redux/favourites/reducer';
+import { CurrentUser, UserResponse } from '@interfaces/authInterfaces.ts';
+
+const middleware = [thunk, fetchMiddleware];
+const mockStore = configureStore(middleware);
+const store = mockStore(initialState);
+
+describe('testCommentsActions', () => {
+  beforeEach(() => {
+    store.clearActions();
+    jest.useFakeTimers();
+  });
+  const MOCKED_DATA: CurrentUser = {
+    bio: 'Esta es una Biografía típica de un usuario !',
+    email: 'hola5@hola.com',
+    id: 209,
+    image:
+      'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+    token:
+      'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MjA5LCJleHAiOjE2MzY5MDMzNzZ9.zN6uihJ-GSArwOpwFUHqLOgNGfTsyIJtoVB4JLlMlTQ',
+    username: 'hola5'
+  };
+  const MOCKED_USER: UserResponse = {
+    user: MOCKED_DATA
+  };
+  test('test GET_FAVORITES_ARTICLES', () => {
+    store.dispatch(actionCreators.getFavouritesArticles(MOCKED_USER));
+    expect(store.getActions()).toEqual([
+      { target: TARGETS.FAVORITES_ARTICLES_LIST, type: actions.FAVORITES_ARTICLES_LIST }
+    ]);
+    expect(store.getActions()).not.toBeNull();
+    expect(store.getActions()).toBeDefined();
+  });
+  test('test GET_FAVORITES_ARTICLES_SUCCESS', () => {
+    store.dispatch(actionCreators.getFavouritesArticles(MOCKED_USER)).then(async () => {
+      expect(await store.getActions()).toEqual([
+        { target: TARGETS.CREATE_COMMENT, type: actions.FAVORITES_ARTICLES_LIST_SUCCESS }
+      ]);
+      expect(store.getActions()).not.toBeNull();
+      expect(store.getActions()).toBeDefined();
+    });
+  });
+});
---
+++ __tests__/redux/favorites/reducer.test.ts
@@ -0,0 +1,52 @@
+import { actions, TARGETS } from '@redux/favourites/actions';
+import CommentReducer, { initialState } from '@redux/favourites/reducer';
+import { CurrentUser } from '@interfaces/authInterfaces.ts';
+
+const MOCKED_DATA: CurrentUser = {
+  bio: 'Esta es una Biografía típica de un usuario !',
+  email: 'hola5@hola.com',
+  id: 209,
+  image:
+    'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+  token:
+    'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MjA5LCJleHAiOjE2MzY5MDMzNzZ9.zN6uihJ-GSArwOpwFUHqLOgNGfTsyIJtoVB4JLlMlTQ',
+  username: 'hola5'
+};
+const favouritesActions = {
+  getFavouritesArticles: {
+    type: actions.FAVORITES_ARTICLES_LIST,
+    target: TARGETS.FAVORITES_ARTICLES_LIST
+  },
+  getFavouritesArticlesSuccess: {
+    type: actions.FAVORITES_ARTICLES_LIST_SUCCESS,
+    target: TARGETS.FAVORITES_ARTICLES_LIST,
+    payload: MOCKED_DATA
+  },
+  getFavouritesArticlesFailure: {
+    type: actions.FAVORITES_ARTICLES_LIST_FAILURE,
+    target: TARGETS.FAVORITES_ARTICLES_LIST,
+    payload: true
+  }
+};
+
+describe('case COMMENTS', () => {
+  test('test FAVORITES_ARTICLES_LIST_INIT', () => {
+    expect(CommentReducer(undefined, favouritesActions.getFavouritesArticles)).toMatchObject({
+      ...initialState,
+      favoritesArticlesListLoading: true
+    });
+  });
+  test('test FAVORITES_ARTICLES_LIST_SUCCESS', async () => {
+    expect(await CommentReducer(undefined, favouritesActions.getFavouritesArticlesSuccess)).toMatchObject({
+      ...initialState,
+      favoritesArticlesList: MOCKED_DATA
+    });
+    expect(await CommentReducer(undefined, favouritesActions.getFavouritesArticlesSuccess)).not.toBeNull();
+  });
+  test('test FAVORITES_ARTICLES_LIST_FAILURE ', async () => {
+    expect(await CommentReducer(undefined, favouritesActions.getFavouritesArticlesFailure)).toMatchObject({
+      ...initialState,
+      favoritesArticlesListError: true
+    });
+  });
+});
