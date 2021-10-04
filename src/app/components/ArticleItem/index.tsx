1f804a716ef22f1de0ae2d0cf9c2afa4e3bfb8c0
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-10-04 17:01:56 -0300

    removed commentLines

--- src/app/components/ArticleItem/index.tsx
+++ src/app/components/ArticleItem/index.tsx
@@ -4,7 +4,6 @@ import CustomText from '@components/CustomText';
 import { Article } from '@interfaces/articlesInterface';
 import { formatDate } from '@utils/dateUtils';
 import icDefaultArticleImage from '@assets/icons/icDefaultArticleImage.jpg';
-// import icDelete from '@assets/icons/icDelete.png';
 import icDelete from '@assets/icons/icTrash.png';
 import { getAvatar } from '@constants/iconsConstants';
 
