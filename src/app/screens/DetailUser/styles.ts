c44795e2f7a3bb0aa7f2c5dec9620d0f60166a15
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-10-06 15:40:50 -0300

    updated detailuser styles

--- src/app/screens/DetailUser/styles.ts
+++ src/app/screens/DetailUser/styles.ts
@@ -1,6 +1,6 @@
+import { StyleSheet } from 'react-native';
 import { BOTTOM_SEPARATOR_LINE } from '@constants/commonStyles';
 import { SPACING } from '@constants/dimentions';
-import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   containerBio: {
