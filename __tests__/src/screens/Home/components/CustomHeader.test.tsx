e49df6c05e8a79132cc7b86b6c7884c44cde2acf
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-10-12 17:17:45 -0300

    updated test

---
+++ __tests__/src/screens/Home/components/CustomHeader.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import MockedNavigator from '@mocks/MockedNavigator';
+
+import CustomHeader from '../../../../../src/app/screens/Home/components/CustomHeader';
+
+jest.mock('react-redux', () => {
+  return {
+    useSelector: jest.fn().mockImplementation(() => []),
+    useDispatch: () => jest.fn()
+  };
+});
+
+describe('test CustomHeaderHome', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+  test('to match with Snapshot', () => {
+    const tree = renderer.create(<MockedNavigator component={CustomHeader} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
