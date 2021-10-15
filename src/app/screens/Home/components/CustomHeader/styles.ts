40a2a2fdf89ca098779678acda6221fc2203d8c3
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-10-15 17:36:24 -0300

    fixed margin tags icon

--- src/app/screens/Home/components/CustomHeader/styles.ts
+++ src/app/screens/Home/components/CustomHeader/styles.ts
@@ -1,22 +1,26 @@
 import { StyleSheet } from 'react-native';
+import { isIos } from '@constants/platform';
 
 const COMMON_LARGE = '100%';
+const CUSTOM_HEIGHT = isIos ? 127 : 73;
+const CUSTOM_POSITION = isIos ? 0 : -50;
+const CUSTOM_MARGIN_TOP = isIos ? '20%' : '8%';
 
 const styles = StyleSheet.create({
   container: {
     width: COMMON_LARGE,
-    height: 120,
-    overflow: 'hidden'
+    height: CUSTOM_HEIGHT
   },
   headerBgStyle: {
     position: 'absolute',
     width: COMMON_LARGE,
-    height: COMMON_LARGE
+    height: 145,
+    top: CUSTOM_POSITION
   },
   logo: {
-    height: 28,
+    height: 21,
     width: COMMON_LARGE,
-    marginTop: '15%'
+    marginTop: CUSTOM_MARGIN_TOP
   }
 });
 
