666e1aa2145d92a605a02fac2d998a575cce7a22
Esteban Indiveri <54692138+EstebanIndiveri@users.noreply.github.com>
2021-10-19 15:31:46 -0300

    Fix title screens (#70)
    * fix styles
    
    * updated test
    
    * added DETAIL_USER:HEADER

--- src/app/screens/FavArticles/styles.ts
+++ src/app/screens/FavArticles/styles.ts
@@ -1,9 +1,6 @@
 import { StyleSheet } from 'react-native';
 import { BOTTOM_SEPARATOR_LINE } from '@constants/commonStyles';
-import { isIos } from '@constants/platform';
-import { green } from '@constants/colors';
 
-const MARGIN_TOP_TITLE = isIos ? 2 : 20;
 const styles = StyleSheet.create({
   container: {
     flex: 1
@@ -12,17 +9,6 @@ const styles = StyleSheet.create({
     ...BOTTOM_SEPARATOR_LINE,
     width: '100%',
     alignItems: 'center'
-  },
-  statusNavBar: {
-    marginTop: MARGIN_TOP_TITLE,
-    width: '100%',
-    alignSelf: 'center',
-    alignItems: 'center',
-    paddingBottom: 5
-  },
-  textTitle: {
-    borderBottomColor: green,
-    borderBottomWidth: 2
   },
   noItemMessage: {
     marginTop: '10%',
