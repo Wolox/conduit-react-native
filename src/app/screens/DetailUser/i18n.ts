304b05dfbe02f924ff39556ab53e63f45b750e42
john07289 <jhon.vente@wolox.co>
2021-10-20 11:04:27 -0500

    update test and validate user

--- src/app/screens/DetailUser/i18n.ts
+++ src/app/screens/DetailUser/i18n.ts
@@ -4,5 +4,7 @@ i18next.addResources('en', 'DETAIL_USER', {
   DESCRIPTION: 'Description',
   DESCRIPTION_BLANK: 'No description yet',
   MY_ARTICLES: 'My Articles',
-  FAVORITED_ARTICLES: 'Favorited Articles'
+  FAVORITED_ARTICLES: 'Favorited Articles',
+  USER_FOLLOW: 'Following',
+  USER_UNFOLLOW: 'Follow'
 });
---
+++ src/app/screens/DetailUser/testIds.ts
@@ -0,0 +1,3 @@
+export default {
+  followUserButton: 'follow-user-button'
+};
